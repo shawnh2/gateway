78d83cfceb47863677d5b53527c8abc40694e9f2
Shawnh2 <shawnhxh@outlook.com>
2023-06-26 22:40:06 +0800

    cf test
    Signed-off-by: Shawnh2 <shawnhxh@outlook.com>

--- internal/gatewayapi/filters.go
+++ internal/gatewayapi/filters.go
@@ -339,26 +339,10 @@ func (t *Translator) processRedirectFilter(
 		}
 	}
 
-	var redirectPort uint32
 	if redirect.Port != nil {
-		redirectPort = uint32(*redirect.Port)
-	} else {
-		if redirect.Scheme != nil {
-			//
-			switch *redirect.Scheme {
-			case "http":
-				redirectPort = 80
-			case "https":
-				redirectPort = 443
-			default:
-				redirectPort = uint32(*filterContext.ParentRef.Port)
-			}
-		} else {
-			//
-			redirectPort = uint32(*filterContext.ParentRef.Port)
-		}
+		redirectPort := uint32(*redirect.Port)
+		redir.Port = &redirectPort
 	}
-	redir.Port = &redirectPort
 
 	filterContext.RedirectResponse = redir
 }
