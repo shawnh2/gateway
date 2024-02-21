8c4ba85cd0f805227c9abaa7485ace39a1ba651b
shawnh2 <shawnhxh@outlook.com>
2024-02-21 11:26:27 +0800

    fix
    Signed-off-by: shawnh2 <shawnhxh@outlook.com>

--- test/e2e/tests/merge_gateways.go
+++ test/e2e/tests/merge_gateways.go
@@ -9,6 +9,7 @@
 package tests
 
 import (
+	"net"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/types"
@@ -22,52 +23,64 @@ func init() {
 }
 
 var MergeGatewaysTest = suite.ConformanceTest{
-	ShortName:   "MergeGateways",
-	Description: "Merge gateways on to a single EnvoyProxy",
+	ShortName:   "BasicMergeGateways",
+	Description: "Basic test for MergeGateways feature",
 	Manifests:   []string{"testdata/merge-gateways/basic-merge-gateways.yaml"},
 	Test: func(t *testing.T, suite *suite.ConformanceTestSuite) {
 		ns := "gateway-conformance-infra"
 
 		route1NN := types.NamespacedName{Name: "merged-gateway-route-1", Namespace: ns}
 		gw1NN := types.NamespacedName{Name: "merged-gateway-1", Namespace: ns}
-		gw1Addr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gw1NN), route1NN)
+		gw1HostPort := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gw1NN), route1NN)
 
 		route2NN := types.NamespacedName{Name: "merged-gateway-route-2", Namespace: ns}
 		gw2NN := types.NamespacedName{Name: "merged-gateway-2", Namespace: ns}
-		gw2Addr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gw2NN), route2NN)
+		gw2HostPort := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gw2NN), route2NN)
 
 		route3NN := types.NamespacedName{Name: "merged-gateway-route-3", Namespace: ns}
 		gw3NN := types.NamespacedName{Name: "merged-gateway-3", Namespace: ns}
-		gw3Addr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gw3NN), route3NN)
+		gw3HostPort := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(t, suite.Client, suite.TimeoutConfig, suite.ControllerName, kubernetes.NewGatewayRef(gw3NN), route3NN)
 
 		t.Run("merged three gateways under the same namespace with http routes", func(t *testing.T) {
+			gw1Addr, _, err := net.SplitHostPort(gw1HostPort)
+			if err != nil {
+				t.Errorf("failed to split hostport %s of gateway %s: %v", gw1HostPort, gw1NN.Name, err)
+			}
+
+			gw2Addr, _, err := net.SplitHostPort(gw2HostPort)
+			if err != nil {
+				t.Errorf("failed to split hostport %s of gateway %s: %v", gw2HostPort, gw2NN.Name, err)
+			}
+
+			gw3Addr, _, err := net.SplitHostPort(gw3HostPort)
+			if err != nil {
+				t.Errorf("failed to split hostport %s of gateway %s: %v", gw3HostPort, gw3NN.Name, err)
+			}
+
 			if gw1Addr != gw2Addr {
-				t.Errorf("failed to merge gateways: inconsistent gateway address %s and %s for %s and %s", gw1Addr, gw2Addr, gw1NN.String(), gw2NN.String())
+				t.Errorf("inconsistent gateway address %s and %s for %s and %s", gw1Addr, gw2Addr, gw1NN.String(), gw2NN.String())
 				t.FailNow()
 			}
 			if gw2Addr != gw3Addr {
-				t.Errorf("failed to merge gateways: inconsistent gateway address %s and %s for %s and %s", gw2Addr, gw3Addr, gw2NN.String(), gw3NN.String())
+				t.Errorf("inconsistent gateway address %s and %s for %s and %s", gw2Addr, gw3Addr, gw2NN.String(), gw3NN.String())
 				t.FailNow()
 			}
 
-			// Three gateways should have the same address.
-			gwAddr := gw1Addr
-
-			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, http.ExpectedResponse{
+			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gw1HostPort, http.ExpectedResponse{
 				Request:   http.Request{Path: "/merge1"},
 				Response:  http.Response{StatusCode: 200},
 				Namespace: ns,
 				Backend:   "infra-backend-v1",
 			})
 
-			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, http.ExpectedResponse{
+			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gw2HostPort, http.ExpectedResponse{
 				Request:   http.Request{Path: "/merge2"},
 				Response:  http.Response{StatusCode: 200},
 				Namespace: ns,
 				Backend:   "infra-backend-v2",
 			})
 
-			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gwAddr, http.ExpectedResponse{
+			http.MakeRequestAndExpectEventuallyConsistentResponse(t, suite.RoundTripper, suite.TimeoutConfig, gw3HostPort, http.ExpectedResponse{
 				Request:   http.Request{Path: "/merge3"},
 				Response:  http.Response{StatusCode: 200},
 				Namespace: ns,
